272dfc6681a2f7376c2164458a97da741788fa2b
bextxy <1779185852@qq.com>
2020-05-29 18:17:59 +0800

    修改按钮权限

--- src/utils/buttonPermission.js
+++ src/utils/buttonPermission.js
@@ -1,6 +1,8 @@
 import store from "../store/index";
 export function buttonPermission(permission){
+    const roles = store.getters["app/roles"]; // 获取角色
     const button = store.getters["app/buttonPermission"];
+    if(roles.includes("admin")) { return true }
     return button.indexOf(permission) != -1; //
 }
 // 2.0
